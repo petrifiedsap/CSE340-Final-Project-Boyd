13c06c456adfd1748a38f245ee3f5676beeb8754
petrifiedsap <petrifiedsap@users.noreply.github.com>
2026-09-17 21:10:15 -0500

    should be set up

---
+++ server.js
@@ -0,0 +1,21 @@
+// Import express using ESM syntax
+import express from 'express';
+
+// Create an instance of an Express application
+const app = express();
+
+//access environmental variable
+const name = process.env.NAME;
+
+// Define a route handler for the root URL ('/')
+app.get('/', (req, res) => {
+    res.send('Hello, ${name}!');
+});
+
+// Define the port number the server will listen on
+const PORT = 3000;
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
